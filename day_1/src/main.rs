061a894ea3b769512176f9ca240b4a8a51248609
Darth-Cube <samuel.ms@gmx.de>
2024-12-02 02:11:08 +0100

    init day 2 project cleanup of day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -22,14 +22,12 @@ fn part_1(file_name: &str) -> io::Result<()> {
     let mut data_1 = BinaryHeap::new();
     let mut data_2 = BinaryHeap::new();
     let mut result = 0;
-    //let mut diff = Vec::new();
 
     for line in reader.lines().map(|l| l.unwrap()) {
         let values: Vec<_> = line.split_whitespace().collect();
         add_to_binheap!(data_1, values[0]); 
         add_to_binheap!(data_2, values[1]); 
     }
-    
     for _ in 0..data_1.len() {
         result += (data_1.pop().unwrap().0).abs_diff(data_2.pop().unwrap().0);
     }
@@ -44,15 +42,13 @@ fn part_2(file_name: &str) -> io::Result<()> {
     let mut data_1 = Vec::new();
     let mut data_2 = Vec::new();
     let mut result = 0;
-    //let mut diff = Vec::new();
 
     for line in reader.lines().map(|l| l.unwrap()) {
         let values: Vec<_> = line.split_whitespace().collect();
         data_1.push(values[0].parse::<u32>().unwrap()); 
         data_2.push(values[1].parse::<u32>().unwrap());
     }
-    for _ in 0..data_1.len() {
-        let value = data_1.pop().unwrap();
+    while let Some(value) = data_1.pop() {
         result += (value) * data_2.iter().filter(|&n|*n == value).count() as u32;
     }
     println!("{}", result);
